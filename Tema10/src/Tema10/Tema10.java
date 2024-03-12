1af06b4e028a114f2ae9b6648ce84afcbf3db0d8
Chad154 <chad154154@gmail.com>
2024-03-12 09:57:44 +0100

    Collection Map HashSet HashMap

---
+++ Tema10/src/Tema10/Tema10.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tema10;
+
+/**
+ *
+ * @author alumno
+ */
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Tema10 {
+    
+    //Para añadir datos en Collection se utiliza .add
+    
+    static Collection rellenar (Collection e){
+        e.add("perro");
+        e.add("gato");
+        e.add("perro");
+        
+        return e;
+    }
+    
+    //Para añadir datos en Map se utiliza .put
+    
+    static Map rellenar (Map m){
+    
+        m.put("perro", "San Bernardo");
+        m.put("gato", "Montes");
+        m.put("perro", "Presa Canario");
+        
+        return m;
+    }
+    
+    public static void main(String[] args){
+        
+        //Se añade java.util.ArrayList
+        System.out.println("ArrayList"+rellenar(new ArrayList()));
+        //Se añade java.util.HashSet
+        System.out.println("HashSet"+rellenar(new HashSet()));
+        //Se añade java.util.HashMap
+        System.out.println("HashMap"+rellenar(new HashMap()));
+        
+    }
+    
+}
