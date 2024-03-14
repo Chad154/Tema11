a62871d935ab26be63d38e0d4fba0d2209b6b360
Chad154 <chad154154@gmail.com>
2024-03-14 12:58:06 +0100

    Iterator 01 Se añade elementos y se modifcan despues utilizando .add .set

--- Tema10/src/Imperador/Iterator01.java
+++ Tema10/src/Imperador/Iterator01.java
@@ -4,6 +4,7 @@
  */
 package Imperador;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,9 +18,42 @@ public class Iterator01 {
         //Iterador en una lista
          
         //Hay que importar la biblioteca Java.util.list
-        Iterable<String> lista01DAM1 = List.of("Alejandro", "David", "Lucia", "Marina");
+        Iterable<String> lista01DAM1 = List.of("Alejandro", "David", "Lucia", "Marina"); 
         //Metodo de Impresion de Iterable
         lista01DAM1.forEach(System.out::println);
+        
+        
+        //Crear el objeto a partir de la clase concreta
+        //A la izquieda el tipo de ArrayList
+        //Hay que importar El Java.util.ArrayList
+        
+        ArrayList<Integer> lista02 = new ArrayList<>();
+        
+        //Ver tipo de lista con get.Class
+        System.out.println("Tipo de lista01" + lista01DAM1.getClass());
+        System.out.println("Tipo de lista02" + lista02.getClass());
+        
+        //Definir el tamaño inical de la Lista
+        //
+        
+        ArrayList<String> lista03 = new ArrayList<>(2);
+        lista03.add("Sara");
+        lista03.add("Juan");
+        System.out.print(lista03 + ",");
+        System.out.println();
+        
+        lista03.add("Elena");
+        System.out.println(lista03 + ",");
+        System.out.println();
+        
+        //Para actualizar elementos ya metidos con .set(index es el numero de elemento y elemento es la modificacion
+        
+        lista03.set(0, "Sara Gonzalez");
+        lista03.set(1,"Juan Torres");
+        lista03.set(2,"Elena Sanchez");
+        
+        System.out.println("Lista03 modificada"+lista03 + ",");
+        System.out.println();
     }
     
 }
