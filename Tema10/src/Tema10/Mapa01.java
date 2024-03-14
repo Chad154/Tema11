882706a53171d3c665816742b88ac3a23d2382c7
Chad154 <chad154154@gmail.com>
2024-03-14 13:24:47 +0100

    Mapa01 Introduccion de datos en Mapa

---
+++ Tema10/src/Tema10/Mapa01.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tema10;
+
+/**
+ *
+ * @author alumno
+ */
+
+import java.util.Map;
+
+public class Mapa01 {
+    
+    public static void main(String[] args) {
+        
+        //Mapa 
+        
+        Map<Integer,String> mapaDAM1 = Map.of(1, "Teclado", 2, "Monitor",3,"CPU");
+        
+        mapaDAM1.forEach((clave,valor) ->System.out.println((clave +":"+valor)) );
+    }
+    
+}
